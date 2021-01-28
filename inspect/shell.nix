98635f937f1bfe58d9c66c4d428c8eab9008b4c2
sdaros <sdaros@cip.li>
2021-01-28 16:51:51 +0100

    Add `inspect` test cases

---
+++ inspect/shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ?
+  import (fetchTarball "http://nixos.org/channels/nixos-20.09/nixexprs.tar.xz")
+  { } }:
+
+let
+  rEnv = (pkgs.rWrapper.override {
+    packages = with pkgs.rPackages;
+      let
+        sqltypes = buildRPackage {
+          name = "sqltypes";
+          src = ../sqltypes;
+          propagatedBuildInputs = [ stringr readr ];
+        };
+      in [ sqltypes dplyr readxl optparse glue readr stringr ];
+  });
+in pkgs.mkShell { buildInputs = [ rEnv ]; }
