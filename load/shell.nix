c55bbfad10256c299b57d585cea9d1389dcd3078
sdaros <sdaros@cip.li>
2021-01-29 14:17:57 +0100

    Rearrange dependencies and add sqlite support and tests

--- load/shell.nix
+++ load/shell.nix
@@ -3,6 +3,11 @@
   { } }:
 
 let
-  pythonEnv =
-    pkgs.python38.withPackages (pypkg: [ pypkg.python-dotenv pypkg.joblib ]);
+  pythonEnv = pkgs.python38.withPackages (pypkg: [
+    pypkg.python-dotenv
+    pypkg.joblib
+    pypkg.boto3
+    pypkg.pyarrow
+    pypkg.sqlalchemy
+  ]);
 in pkgs.mkShell { buildInputs = [ pythonEnv ]; }
