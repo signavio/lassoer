462e080f0334b56a530209e9da574d3a2e21c2fe
sdaros <sdaros@cip.li>
2021-02-08 14:00:11 +0100

    Add default.nix for anonymize

---
+++ anonymize/default.nix
@@ -0,0 +1,21 @@
+{ pkgs ?
+  import (fetchTarball "http://nixos.org/channels/nixos-20.09/nixexprs.tar.xz")
+  { } }:
+
+let
+  nodeDependencies =
+    (pkgs.callPackage ./node-composition.nix { }).shell.nodeDependencies;
+
+in pkgs.stdenv.mkDerivation {
+  name = "anonymize";
+  version = "0.2.0";
+  src = ./.;
+  buildInputs = [ pkgs.nodejs ];
+  installPhase = ''
+    mkdir -p $out/bin
+    ln -s ${nodeDependencies}/lib/node_modules $out/bin/node_modules
+    export PATH="${nodeDependencies}/bin:$PATH"
+
+    cp anonymize $out/bin/
+  '';
+}
