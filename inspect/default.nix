8f9621bad0b34451dd2b0392230dc7cc32fe0ca9
sdaros <sdaros@cip.li>
2021-02-08 16:30:01 +0100

    Add default.nix to inspect

---
+++ inspect/default.nix
@@ -0,0 +1,30 @@
+{ pkgs ?
+  import (fetchTarball "http://nixos.org/channels/nixos-20.09/nixexprs.tar.xz")
+  { } }:
+
+let
+  rEnv = pkgs.rWrapper.override {
+    packages = with pkgs.rPackages;
+      let
+        sqltypes = buildRPackage {
+          name = "sqltypes";
+          src = ../sqltypes;
+          propagatedBuildInputs = [ stringr readr ];
+        };
+      in [ sqltypes dplyr readxl optparse glue readr stringr ];
+  };
+in pkgs.stdenv.mkDerivation {
+  name = "inspect";
+  version = "0.3.0";
+  src = ./.;
+  buildInputs = [ pkgs.file rEnv ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp inspect $out/bin
+  '';
+  doInstallCheck = true;
+  installCheckPhase = ''
+    PATH=$PATH:$out/bin
+    ${pkgs.bash}/bin/bash tests/test_inspect
+  '';
+}
