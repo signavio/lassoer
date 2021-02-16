b8821021f38b7a01f0cd2126e9179c7b99cfbf57
sdaros <sdaros@cip.li>
2021-02-16 21:48:25 +0100

    Fix the way we parse target db url

--- load/load.py
+++ load/load.py
@@ -13,15 +13,17 @@ import boto3, csv, re, os, json, io, sys, sqlite3
 import pyarrow as pa
 
 parser = OptionParser(
-    usage="%prog [options] target file",
+    usage="%prog [options] file",
     description="Load a CSV file into a target sql database",
     version="%prog v0.3.0",
     epilog="Load expects to find a .metadata file for the input file "  +
     "describing the schema of the .csv file in SQL DDL format. This file " +
-    "can be generated using the `inspect` command in this repo"
+    "can be generated using the `inspect` command in this repo. " +
+    "The target sql database is sourced from $DATABASE_URL " +
+    "in the environment file"
 )
 parser.add_option("-d", "--drop-if-exists", dest="drop", action="store_true",
-                  help="Drop existing database schema/tables before creating new")
+                  help="Drop existing database table before creating new")
 parser.add_option("-f", "--environment-file", dest="environment_file",
                   help="Location of environment file containing credentials",
                   default=Path('.') / '.env')
@@ -32,10 +34,10 @@ env = dotenv_values(options.environment_file)
 
 def load_into_sqlite(env, options, target, csv_file):
         path_to_db_file = target[9:]
-        (table_name, _) = splitext(basename(csv_file))
+        table_name = basename(csv_file).split('.')[0]
         sanitized_table_name = re.sub(r"-|\s", '_', table_name)
         if options.drop:
-            os.remove(path_to_db_file)
+            cur.execute(f'DROP TABLE IF EXISTS "{sanitized_table_name}"')
         con = sqlite3.connect(path_to_db_file)
         cur = con.cursor()
         create_stmt = create_stmt_ddl_from(f"{csv_file}.metadata") 
@@ -58,17 +60,13 @@ def load_into_sqlite(env, options, target, csv_file):
 def load_into_postgresql(env, options, target, csv_file):
     create_stmt = create_stmt_ddl_from(f"{csv_file}.metadata")
     db_uri = target
-    (table_name, _) = splitext(basename(csv_file))
+    table_name = basename(csv_file).split('.')[0]
     engine = create_engine(db_uri)
     conn = engine.connect()
     if options.drop:
-        drop_schema_then_create = text(f'\
-        DROP SCHEMA IF EXISTS "{options.db_schema}" CASCADE;\
-        CREATE SCHEMA "{options.db_schema}";')
-        conn.execute(drop_schema_then_create)
-    if options.db_schema:
-        conn.execute(f'SET search_path TO "{options.db_schema}";')
-    create_stmt = f'CREATE TABLE IF NOT EXISTS {table_name} ( ' + create_stmt + ";"
+        conn.execute(f'DROP TABLE IF EXISTS "{options.db_schema}".{table_name};')
+    conn.execute(f'CREATE SCHEMA IF NOT EXISTS "{options.db_schema}";')
+    create_stmt = f'CREATE TABLE IF NOT EXISTS "{options.db_schema}".{table_name} ( ' + create_stmt + ";"
     conn.execute(create_stmt)
     psql_copy(db_uri, options.db_schema, csv_file)
 
@@ -78,21 +76,20 @@ def create_stmt_ddl_from(csv_metadata_file):
         reader = csv.DictReader(csvfile, delimiter="|")
         first_row = next(reader)
         if first_row["data_type"].startswith("DECIMAL"):
-            create_stmt = f'{first_row["column_name"]} {first_row["data_type"]},'   
+            create_stmt = f'"{first_row["column_name"]}" {first_row["data_type"]},'   
         else:
-            create_stmt = f'{first_row["column_name"]} {re.findall("[^(]+", first_row["data_type"])[0]},'
+            create_stmt = f'"{first_row["column_name"]}" {re.findall("[^(]+", first_row["data_type"])[0]},'
         for row in reader:
             if row["data_type"].startswith("DECIMAL"):
-                create_stmt += f' {row["column_name"]} {row["data_type"]},'
+                create_stmt += f' "{row["column_name"]}" {row["data_type"]},'
             else:
-                create_stmt += f' {row["column_name"]} {re.findall("[^(]+", row["data_type"])[0]},'
+                create_stmt += f' "{row["column_name"]}" {re.findall("[^(]+", row["data_type"])[0]},'
         create_stmt = create_stmt[:-1] + ')'
     return create_stmt
     
 def psql_copy(db_uri, db_schema, csv_file):
-   
-   (table_name, _) = splitext(basename(csv_file))
-   psql_copy_cmd = [
+    table_name = basename(csv_file).split('.')[0]
+    psql_copy_cmd = [
     'psql', db_uri,
     '-c', f'\COPY "{db_schema}".{table_name} ' +\
     f'FROM {csv_file} WITH ' +\
@@ -102,12 +99,12 @@ def psql_copy(db_uri, db_schema, csv_file):
     "QUOTE AS '\"' " +\
     "ESCAPE AS '\"'"
     ]
-   process = Popen(psql_copy_cmd, stdout=PIPE, stderr=PIPE)
-   stdout, stderr = process.communicate()
-   if len(stdout) > 0:
+    process = Popen(psql_copy_cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if len(stdout) > 0:
        rows_copied = stdout.decode("utf-8")[:-1] 
        print(f'{rows_copied} rows from {csv_file} to "{db_schema}".{table_name}')
-   if len(stderr) > 0:
+    if len(stderr) > 0:
        print(stderr.decode("utf-8")[:-1])
 
 def create_pyarrow_schema(column_names, data_types):
@@ -171,7 +168,8 @@ def load_into_athena(env, path_to_csv_file):
         aws_access_key_id = env["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key = env["AWS_SECRET_ACCESS_KEY"]
     )
-    (table_name, _) = splitext(basename(path_to_csv_file))
+
+    table_name = basename(csv_file).split('.')[0]
     parquet = as_parquet(path_to_csv_file)
     s3 = aws_session.resource('s3')
     print(f'Copy from {path_to_csv_file}.parquet to s3://{env["BUCKET_NAME"]}/{table_name}/{basename(path_to_csv_file)}.parquet')
@@ -205,19 +203,26 @@ def load_into_athena(env, path_to_csv_file):
     print(json.dumps(response,indent=2))
 
 def run():
-    if len(args) != 2:
+    if len(args) != 1:
         parser.print_help()
         sys.exit(1)
-    target = args.pop(0)
     csv_file = args.pop(0)
+    try:
+        target = env["DATABASE_URL"]
+    except KeyError:
+        print("The environment file must specify a valid $DATABASE_URL variable")
+        sys.exit(1)
     if target.startswith("postgresql"):
+        if options.db_schema is None:
+            print("A Database schema must be provided using option -d")
+            sys.exit(1)
         load_into_postgresql(env, options, target, csv_file)
     elif target.startswith("athena"):
         load_into_athena(env, csv_file)
     elif target.startswith("sqlite"):
         load_into_sqlite(env, options, target, csv_file)
     else:
-        print(f"The database of type `{options.db_target.split('/')[:-1]}` is not supported")
+        print(f"The database of type `{target.split('/')[:-1]}` is not supported")
 
 if __name__ == "__main__":
     run()
